6e509ad78c6724a3a37c0b2410ba1104f0aabee1
JessicaMeixner-NOAA <Jessica.Meixner@noaa.gov>
2022-11-21 08:58:20 -0600

    add setup script

---
+++ setupgrids.sh
@@ -0,0 +1,18 @@
+
+#a script to copy in grids here because they are too large to be stored in git: 
+
+
+srcdir=/work2/noaa/marine/jmeixner/unstructuredgrids/fix_grids
+cp ${srcdir}/unst_glo_100km.msh   grids/glo_100km/
+cp ${srcdir}/unst_glo_15km.msh grids/glo_15km/
+cp ${srcdir}/unst_glo_20km.msh grids/glo_20km/
+cp ${srcdir}/unst_glo_coast3km.msh grids/glo_coast3km/
+cp ${srcdir}/unst_glo_depthB.msh grids/glo_depthB/
+cp ${srcdir}/unst_glo_depthC.msh grids/glo_depthC/
+cp ${srcdir}/unst_glo_m1gfsv16.msh grids/glo_m1gfsv16/
+cp ${srcdir}/unst_glo_multi1.msh grids/glo_multi1/
+cp ${srcdir}/unst_tri_15min.msh grids/tri_15min/
+
+
+
+
